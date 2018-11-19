06a66432e6ee736fa457481c938a59a37c4505f7
yaniv <you@example.com>
2018-11-19 08:58:56 +0200

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bb780989216aebf7141d.js"
+    "url": "webpack-runtime-b69b5cf48376fc8eebf1.js"
   },
   {
     "url": "app-b476896880e3ad7e5199.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9d1fc06c4558d567e31538d2abe8bfbb"
+    "revision": "a51fb047a1e4d202bf1248c9306a99d7"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
